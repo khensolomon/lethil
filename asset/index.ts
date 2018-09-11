6ae415b93d81238cfbb17516c905d3b72cc7ad78
Khen Solomon Lethil <khensolomon@gmail.com>
2018-09-11 02:33:24 +0200

    removed NOTE

--- asset/index.ts
+++ asset/index.ts
@@ -14,8 +14,9 @@ export const path=root.request.path;
 export const fs=root.request.fs;
 
 // NOTE: Assignment
-export const assignment:any = {};
+const assignment:any = {};
 // const assignment:any = module.exports = root;
+// const assignment:any = module.exports = {rootConfiguration,path,fs,rootAssist,rootObject,rootArray};
 
 // NOTE:  Scriptive
 import * as $ from './scriptive';
@@ -30,38 +31,12 @@ export const sassMiddleWare = $.sassMiddleWare;
 // TODO: ?
 export const httpErrors = $.httpErrors;
 
-// console.log(scriptive);
-// scriptive.serve.http
-// const {expressVirtual,express,environments,debug} = require('./virtual');
-// const expressTest = require('./express-test');
-// const app = express();
-/*
-const {server} = require('@scriptive/evh');
-scriptive
-rootProject, rootHttp, rootServer
-var server = new expressVirtual;
-// NOTE: Optional
-// server.port(3000);
-// server.root(__dirname);
-server.start();
-server.error();
-server.listening();
-*/
-// NOTE: Configurations
-// const assignment:any = module.exports = {rootConfiguration,path,fs,rootAssist,rootObject,rootArray};
-
-// export const assignment:any = {config};
-// export default assignment;
-
-
 // TODO: ??
 import * as database from './database';
-// assignment.mysql = database.connection.mysql;
 export const mysql = database.connection.mysql;
-// assignment.mongodb = database.connection.mongodb;
 export const mongodb = database.connection.mongodb;
-// assignment.testing = database.connection.testing;
 
 export const world:string = 'world';
 export const hello:string = 'Hello';
+
 export default assignment;
