30f4ab602506dca24207e1168b24f227f519d7c5
Khen Solomon Lethil <khensolomon@gmail.com>
2020-12-08 09:17:00 +0100

    change url

--- database/mongo.js
+++ database/mongo.js
@@ -2,16 +2,16 @@
 
 module.exports = class database {
   constructor(e) {
-    this.url = e.hasOwnProperty('mongoConnection')?e.mongoConnection:null;
+    this.config = e.hasOwnProperty('mongoConnection')?e.mongoConnection:null;
     this.connection = null;
     this.factor = null;
   }
 
-  async connect(url){
-    if (!this.connection || url) {
-      this.url = url || this.url;
+  async connect(config){
+    if (!this.connection || config) {
+      this.config = config || this.config;
       // this.connection = await MongoClient.connect(this.url,{ useNewUrlParser: true,useUnifiedTopology: true });
-      this.connection = await this.factor.MongoClient.connect(this.url,{ useNewUrlParser: true,useUnifiedTopology: true });
+      this.connection = await this.factor.MongoClient.connect(this.config,{ useNewUrlParser: true,useUnifiedTopology: true });
     }
     return this.connection;
   }
