f6678fcf92e1045acfd82e60e2c3b10ca1f15866
Khen Solomon Lethil <khensolomon@gmail.com>
2021-01-16 08:47:01 +0100

    Update definition

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,28 +1,18 @@
-/// <reference path="./service/index.d.ts" />
-
-import service from "./service/index";
-
-export const aid = service.aid;
-export const utility = service.utility;
-export const ask = service.ask;
-export const timer = service.timer;
-export const burglish = service.burglish;
-
-namespace scriptive {
+declare namespace lethil {
 
   /**
    * configuration
    */
   function set(name?:string,rest?:any): any;
 
-  function db(): $.db;
-  function config(): $.config.user;
+  function db(): any;
+  function config(): any;
 
   /**
    * set tasks or routes
    * @returns $.Route
    */
-  function route(name?:string,rest?:any): $.Route;
+  function route(name?:string,rest?:any): any;
 
   /**
    * void
@@ -44,12 +34,7 @@ namespace scriptive {
   function server(): any;
 
   interface PlatformPath {
-    abc:number
   }
 }
 
-export default scriptive;
-// const scriptive: scriptive.PlatformPath;
-
-
-// export = scriptive;
+export default lethil;
