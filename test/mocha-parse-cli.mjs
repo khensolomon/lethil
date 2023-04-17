326c053c554e443b65094615b6b50b185cb8f411
Khen Solomon Lethil <khensolomon@gmail.com>
2023-04-17 17:56:18 +0200

    add parse.cli

---
+++ test/mocha-parse-cli.mjs
@@ -0,0 +1,85 @@
+import "mocha";
+import * as assert from "assert";
+import { parse } from "../lethil.mjs";
+
+describe("parse.cli", function () {
+  // process.argv.splice(2);
+  it("npm run script param", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ");
+    let job = parse.cli(splice);
+    assert.strictEqual("/npm/run/script/param", job);
+  });
+
+  it("node run third third", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ");
+    let job = parse.cli(splice);
+    assert.strictEqual("/node/run/third/third", job);
+  });
+
+  it("node run first second", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/first/second", job);
+    assert.ok(true);
+  });
+
+  it("node run none --second", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/none", job);
+  });
+
+  it("node run first --q=love k=key", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/first?q=love&k=key", job);
+  });
+
+  it("node run first --q=love --k=key", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/first?q=love&k=key", job);
+  });
+
+  it("node run first?q:love --k=key", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/first?q=love&k=key", job);
+  });
+
+  it("node run first?q=love&k=key", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/first?q=love&k=key", job);
+  });
+
+  it("node run first q=love k=key", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/first/q=love/k=key", job);
+  });
+
+  it("npm run none-param --q --k", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/none-param", job);
+  });
+
+  it("npm run gist request list --file /abc/org boolen false", function () {
+    let title = this.test?.title;
+    let splice = title?.split(" ").splice(2);
+    let job = parse.cli(splice);
+    assert.strictEqual("/gist/request/list?file=/abc/org&boolen=false", job);
+    // console.log(job);
+  });
+});
