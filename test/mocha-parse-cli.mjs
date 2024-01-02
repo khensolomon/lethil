40df7091edeef85fe1432f8ae8fb31594a2c433a
Khen Solomon Lethil <khensolomon@gmail.com>
2024-01-02 10:00:57 +0100

    fixed arguments with space

--- test/mocha-parse-cli.mjs
+++ test/mocha-parse-cli.mjs
@@ -4,6 +4,34 @@ import { parse } from "../lethil.mjs";
 
 describe("parse.cli", function () {
   // process.argv.splice(2);
+
+  it("bible search tedim --q 'Topa kiangah'", function () {
+    let title = this.test?.title;
+    let job = parse.cli(title);
+    // const route = parse.url(job);
+    // console.log(route);
+    assert.strictEqual("/bible/search/tedim?q=Topa kiangah", job);
+    // console.log(job);
+  });
+  it("bible search tedim --q 'Topa kiangah' b=0", function () {
+    let title = this.test?.title;
+    let job = parse.cli(title);
+    // const route = parse.url(job);
+    // console.log(route);
+    assert.strictEqual("/bible/search/tedim?q=Topa kiangah&b=0", job);
+    // console.log(job);
+  });
+  it("bible search tedim --q Topa kiangah", function () {
+    let title = this.test?.title;
+    let job = parse.cli(title);
+    assert.strictEqual("/bible/search/tedim?q=Topa kiangah", job);
+  });
+  it("bible search tedim?q=Topa kiangah", function () {
+    let title = this.test?.title;
+    let job = parse.cli(title);
+    assert.strictEqual("/bible/search/tedim?q=Topa kiangah", job);
+  });
+
   it("npm run script param", function () {
     let title = this.test?.title;
     let splice = title?.split(" ");
@@ -68,29 +96,37 @@ describe("parse.cli", function () {
     assert.strictEqual("/first/q=love/k=key", job);
   });
 
-  it("npm run none-param --q a --k b", function () {
+  it("npm run none-param --q a --k=b", function () {
     let title = this.test?.title;
     let splice = title?.split(" ").splice(2);
     let job = parse.cli(splice);
     assert.strictEqual("/none-param?q=a&k=b", job);
   });
 
-  it("npm run gist request list --file /abc/org boolen false", function () {
+  it("npm run gist request list --file /abc/org boolen=false", function () {
     let title = this.test?.title;
     let splice = title?.split(" ").splice(2);
     let job = parse.cli(splice);
     assert.strictEqual("/gist/request/list?file=/abc/org&boolen=false", job);
   });
 
-  it("bible search tedim --keyword='Topa kiangah'", function () {
+  it("bible search tedim?q=Topa", function () {
+    let title = this.test?.title;
+    // let splice = title?.split(" ").splice(2);
+    let job = parse.cli(title);
+    assert.strictEqual("/bible/search/tedim?q=Topa", job);
+    // console.log(job);
+  });
+
+  it("bible search tedim --q='Topa kiangah'", function () {
     // node run bible search tedim --keyword='Topa kiangah'
     let title = this.test?.title;
     // let splice = title?.split(" ").splice(2);
     let job = parse.cli(title);
-    assert.strictEqual("/bible/search/tedim?keyword=Topa kiangah", job);
+    assert.strictEqual("/bible/search/tedim?q=Topa kiangah", job);
     // console.log(job);
   });
-  it("bible search tedim --keyword='k w' k='a b'", function () {
+  it("bible search tedim --keyword='k w' k=a b", function () {
     // node run bible search tedim --keyword='Topa kiangah'
     let title = this.test?.title;
     // let splice = title?.split(" ").splice(2);
@@ -106,15 +142,14 @@ describe("parse.cli", function () {
     assert.strictEqual("/bible/search/tedim?keyword=k w&k=a b", job);
     // console.log(job);
   });
-  it("bible search tedim?keyword='k w' k='a b'", function () {
-    // node run bible search tedim --keyword='Topa kiangah'
+  it("bible search tedim?q='k w' k='a b'", function () {
+    // node run bible search tedim --q='Topa kiangah'
     let title = this.test?.title;
     // let splice = title?.split(" ").splice(2);
     let job = parse.cli(title);
-    assert.strictEqual("/bible/search/tedim?keyword=k w&k=a b", job);
-    // console.log(job);
+    assert.strictEqual("/bible/search/tedim?q=k w&k=a b", job);
   });
-  it("reference?q='Gen.1:3;Gen.2:3' a:b", function () {
+  it("reference?q='Gen.1:3;Gen.2:3'", function () {
     // node run bible search tedim --q='Gen.1:3;Gen.2:3'
     let title = this.test?.title;
     // let splice = title?.split(" ").splice(2);
